58c768eb8da7c12c4fbd5cf043a3432329d5aaed
Benjamin Rose <benthere914@gmail.com>
2021-07-21 13:16:18 -0500

    still some bugs - working on them

--- player.js
+++ player.js
@@ -1,25 +1,24 @@
 const {ParentItem, Food, CombatItem, Weapon, Armor, helmet, breastPlate, leggings, boots, sword, dagger, apple, sandwich} = require("./items")
 
 class Character{
-    constructor(name, health, hunger, weaponEquipped, armorEquipped){
+    constructor(name, health, hunger, weaponEquipped, armorEquipped, weaponSelected){
         this.name = name,
         this.health = health,
         this.hunger = hunger;
         this.weaponEquipped = weaponEquipped,
+        this.weaponSelected= weaponSelected,
         this.armorEquipped = armorEquipped
     }
+
     giveDamage(amount, player){player.takeDamage(amount)}
     takeDamage(amount){this.health -= amount}
-    heal(amount){for(let i = 0; i <= amount && this.health < 100; i++){this.health++;}}
-    equipWeapon(weapon){this.weaponEquipped = weapon;}
-    unequipWeapon(){this.weaponEquipped = undefined}
-    pickUpItem(item){this.inventory.push(item)}
-    dropItem(itemId){this.inventory.forEach((ele, i) => {if (ele.id === itemId){this.inventory.splice(i,1)}})}
     eat(food){
         if (food instanceof Food) {
             if (this.inventory.includes(food)){
-                for (let i = 0; i <= food.hungerPoints && this.hunger < 100; i++){this.hunger++}
-                for (let i = 0; i <= food.healthPoints && this.health < 100; i++){this.health++}
+                this.heal(food.hungerPoints, this.hunger);
+                this.heal(food.healthPoints, this.health)
+                // for (let i = 0; i <= food.hungerPoints && this.hunger < 100; i++){this.hunger++}
+                // for (let i = 0; i <= food.healthPoints && this.health < 100; i++){this.health++}
                 this.dropItem(food.id)
             }else {
                 console.log(`You can only eat food that you have picked up.`)
@@ -28,12 +27,28 @@ class Character{
             console.log(`${food.name} is not food.`)
         }
     }
+    heal = (amount, type) => {for(let i = 0; i <= amount && type < 100; i++){type++;}}
+
+    equipWeapon(weapon){
+        if (!weapon) {weapon = this.weaponSelected}
+        if (weapon instanceof Weapon) {
+            if (this.inventory.includes(weapon)){
+                this.weaponSelected = weapon;this.weaponEquipped = weapon;
+            }else {console.log(`${weapon.name} is not in your inventory`)}
+        }else {console.log(`${weapon.name} is not a weapon`)}
+    }
+    unequipWeapon(){this.weaponSelected = this.weaponEquipped; this.weaponEquipped = undefined}
+
+    pickUpItem(item){this.inventory.push(item)}
+    dropItem(itemId){this.inventory.forEach((ele, i) => {if (ele.id === itemId){this.inventory.splice(i,1)}})}
+
+
 }
 
 
 class Player extends Character{
-    constructor(name, health, hunger, weaponEquipped, armorEquipped, inventory = []){
-        super(name, health, hunger, weaponEquipped, armorEquipped)
+    constructor(name, health, hunger, weaponEquipped, armorEquipped = [undefined, undefined, undefined, undefined], inventory = [], weaponSelected){
+        super(name, health, hunger, weaponEquipped, armorEquipped, weaponSelected);
         this.inventory = inventory;
     }
 }
@@ -42,17 +57,18 @@ class Player extends Character{
 
 const ben = new Player("Ben", 100, 100);
 const bob = new Player("Bob", 100, 100);
-console.log(ben)
+console.log(ben.health)
 bob.giveDamage(20, ben);
 ben.pickUpItem(apple)
-console.log(ben)
-ben.pickUpItem(sword)
+// ben.pickUpItem(sword)
+console.log(ben.health)
 ben.eat(apple)
-ben.eat(sword)
-
-// console.log(ben)
+// ben.eat(sword)
+// ben.equipWeapon(sword)
+console.log(ben.health)
+// ben.unequipWeapon();
 // console.log("pause")
 // ben.dropItem("apple01")
 // ben.heal(25)
-console.log(ben.inventory)
-console.log(ben)
+// console.log(ben.inventory)
+// console.log(ben)
