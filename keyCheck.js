6b92e032dfe5be123608f89dcdfcbe4b08c50898
Benjamin Rose <benthere914@gmail.com>
2021-07-22 22:37:41 -0500

    added mouse click functionality

--- keyCheck.js
+++ keyCheck.js
@@ -3,6 +3,9 @@ let keypress = require("keypress")
 keypress(process.stdin);
 
 function keyCheck(character, output = false){
+  process.stdin.setRawMode(true);
+
+  process.stdin.resume();
 process.stdin.on('keypress', function (ch, key) {
   if (output){
   console.log('got "keypress"', key);
@@ -11,11 +14,31 @@ process.stdin.on('keypress', function (ch, key) {
     process.stdin.pause();
   }
   if (key && key.name === character){
+      process.stdin.pause();
       return true
   }
 });}
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
 
-module.exports = {keyCheck};
+function getClick(showCoord){
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  keypress.enableMouse(process.stdout);
+  process.stdin.on('mousepress', function (info) {
+    if (showCoord){console.log(`x: ${info.x}, y: ${info.y}`)}
+    process.stdin.pause();
+    // console.log(info)
+    if (info.button){return info.button}
+    if (info.scroll){return info.scroll}
+  // console.log('got "mousepress" event at %d x %d', info.x, info.y);
+  });
+  // if (true){
+  process.on('exit', function () {
+  // disable mouse on exit, so that the state
+  // is back to normal for the terminal
+  keypress.disableMouse(process.stdout);
+  });
+}
+getClick(true)
+keyCheck("up")
+module.exports = {keyCheck, getClick};
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {ParentItem, Food, CombatItem, Weapon, Armor} = require("./items")
 const {Character, Player} = require('./player')
 const {World} = require('./world')
+const {keyCheck, getClick} = require("./keyCheck")
 
 
 module.exports = {Character, Player, World, ParentItem, Food, CombatItem, Weapon, Armor}
