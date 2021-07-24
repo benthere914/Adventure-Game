4093904641de52c52a5f5d0d0449d8911bfbf7e9
Benjamin Rose <benthere914@gmail.com>
2021-07-23 19:26:36 -0500

    still has some bugs

--- mouse.js
+++ mouse.js
@@ -1,6 +1,5 @@
 let keypress = require("keypress")
 keypress(process.stdin);
-keypress.enableMouse(process.stdout);
 
 function keyCheck(print = false){
     let output = [];
@@ -18,14 +17,16 @@ function keyCheck(print = false){
                 }
                 if (!notAllowed.includes(key.name)){
                     output.push(key.name);
-                    process.stdout.write(key.name);
+                    // process.stdout.write(key.name);
                 }
+                if (key.name === "space"){output.push(" ")};
             }
             if (key.name === 'return' || key.name === 'enter') {
                 output = output.join("");
-                console.log("");
+                // console.log("");
                 console.log(`you typed ${output}`);
-                process.stdin.pause();
+                // process.stdin.pause();
+                output = [];
                 return output}
         }
   });
@@ -45,18 +46,70 @@ function getClick(output = false){
     });
     process.on('exit', function () {keypress.disableMouse(process.stdout);});
 }
-// async function test(variable) {
-//     variable = await keyCheck()
-//     variable = await getClick()
-//     return variable
-// }
-// async function testb(params) {
-//     await console.log(test())
-// }
-// testb()
-
-async function getStr(...rest) {
-    const str = await keyCheck(1)
+function name(params) {
+return keyCheck()
 }
 
-getStr()
+
+
+// getClick(1)
+
+console.log("after the code above")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let res = keyCheck();
+// console.log(res)
+
+
+// function playGame(){
+//     let output = [];
+//     let notAllowed = ["space", "right" ,"left" ,"down" ,"up"];
+//     let gameState = "start";
+//     getClick()
+//     console.log("What is your name?");
+//     process.stdin.resume();
+//     process.stdin.on('keypress', function (ch, key) {
+//         if (key){
+//             if (key.ctrl && key.name == 'c') {
+//                 process.stdin.pause();
+
+//             }
+//             else if (key.name !== 'return' && key.name !== 'enter') {
+//                 if (!notAllowed.includes(key.name)){
+//                     output.push(key.name);
+//                     process.stdout.write(key.name);
+//                 }
+//                 if (key.name === "space"){output.push(" ")};
+//             }
+//             else if (key.name === 'return' || key.name === 'enter') {
+//                 resultStr = output.join("");
+//                 console.log("");
+//                 // console.log(`Hi there, ${output}`);
+//                 output = [];
+//             }
+//             function game(...params) {
+
+//             }
+//         if (gameState === "start"){}
+//             if (pressed){
+//                 console.log("hello, ", resultStr)
+//             }
+//         }
+
+
+//     })
+// }
+// playGame()
--- world.js
+++ world.js
@@ -1,4 +1,5 @@
-const {Character, Player, ben} = require("./player")
+const {Character, Player} = require("./player")
+const {ParentItem, Food, CombatItem, Weapon, Armor} = require('./items')
 class World{
     constructor(name, length, width, height, containers = [], characters = []){
         this.name = name,
@@ -26,11 +27,27 @@ class World{
     }
 }
 
-// ben.init()
-// let room1 = new World("Merchant bobs store", 25, 25, 9, [], []);
-// room1.addCharacter(ben)
-// ben.walkNorth(5);
-// console.log(room1)
+const helmet = new Armor("Iron Helmet", 25, "h01" ,75, true, 30, "helmet");
+
+const breastPlate = new Armor("Iron Breast Plate", 35, "bp01", 75, true, 55, "breastPlate");
+const leggings = new Armor("Iron Leggings", 30, "l01" ,75, true, 40, "leggings");
+const boots = new Armor("Iron Boots", 25, "b01", 75, true, 20, "boots");
+
+const sword = new Weapon("Iron Sword", 80, "s01", 75, true, 10, "sword");
+const dagger = new Weapon("Iron Dagger", 15, "d01", 75, true, 5, "dagger");
+const fists = new Weapon("Just Regular punching fists", NaN, "fists", Infinity, true, 2, "fists" )
+
+const apple = new Food("Red Apple", 2, "apple01", 1, 2);
+const sandwich = new Food("BLT", 3.5, "sandwich01", 2, 4)
+
+
+const ben = new Player("Ben", 100, 100, sword, [helmet, breastPlate, leggings, boots] );
+ben.init()
+let room1 = new World("Merchant bobs store", 25, 25, 9, [], []);
+room1.addCharacter(ben)
+ben.walkNorth(5);
+ben.walkEast(17)
+console.log(room1)
 
 
 module.exports = {World}
